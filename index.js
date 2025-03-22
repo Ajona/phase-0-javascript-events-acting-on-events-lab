7149c63b118e32b583590c62fe77f3d5cdb65dbe
Warioba <ajonawarioba@gmail.com>
2025-03-22 14:20:01 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Your code here
+
+document.addEventListener("keydown", function (event) {
+    console.log(event);
+  });
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+      const leftNumbers = dodger.style.left.replace("px", "");
+      const left = parseInt(leftNumbers, 10);
+      dodger.style.left = `${left - 1}px`;
+    }
+  });
+
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if (left < 360) {  // 400px game area - 40px dodger
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
